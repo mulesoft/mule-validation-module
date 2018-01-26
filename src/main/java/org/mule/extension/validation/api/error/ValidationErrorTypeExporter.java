02993a5b3c6db92382ac69f59eadbb08e436fb8a
Nicolas Di Nucci <nico.dinu@gmail.com>
2018-01-26 15:59:19 -0300

    MULE-14551: ValidationErrorType needs to be exported so a user can create a custom Validator

---
+++ src/main/java/org/mule/extension/validation/api/error/ValidationErrorTypeExporter.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.extension.validation.api.error;
+
+/**
+ * The only porpouse of this class is to export the {@link ValidationErrorType} with the {@link org.mule.runtime.extension.api.annotation.Export}
+ * annotation.
+ */
+public class ValidationErrorTypeExporter {
+}
