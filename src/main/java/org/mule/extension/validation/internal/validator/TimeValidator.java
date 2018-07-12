8ab30f564be2ae8877a1f05d90e6122d9233920f
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2018-07-12 14:33:31 -0300

    MULE-15342: Validation module is-time operation accepts invalid dates (#71)

--- src/main/java/org/mule/extension/validation/internal/validator/TimeValidator.java
+++ src/main/java/org/mule/extension/validation/internal/validator/TimeValidator.java
@@ -15,6 +15,8 @@ import org.mule.runtime.api.i18n.I18nMessage;
 
 import java.util.Locale;
 
+import org.joda.time.format.DateTimeFormat;
+
 /**
  * An {@link AbstractValidator} which verifies that a {@link #time} represented as a {@link String} can be parsed using a given
  * {@link #locale} and {@link #pattern}
@@ -37,19 +39,12 @@ public class TimeValidator extends AbstractValidator {
 
   @Override
   public ValidationResult validate() {
-    org.apache.commons.validator.routines.TimeValidator validator =
-        org.apache.commons.validator.routines.TimeValidator.getInstance();
     Locale locale = new Locale(this.locale);
-    if (pattern != null) {
-      if (!validator.isValid(time, pattern, locale)) {
-        errorMessage = getMessages().invalidTime(time, this.locale, pattern);
-        return fail();
-      }
-    } else {
-      if (!validator.isValid(time, locale)) {
-        errorMessage = getMessages().invalidTime(time, this.locale, pattern);
-        return fail();
-      }
+    try {
+      DateTimeFormat.forPattern(pattern).withLocale(locale).parseDateTime(time);
+    } catch (IllegalArgumentException e) {
+      errorMessage = getMessages().invalidTime(time, this.locale, pattern);
+      return fail();
     }
 
     return ok();
