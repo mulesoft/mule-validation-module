3ee76a9db4f5e9d5fe2b0b1a293dada4a0cd8412
Francisco Nery <fnery@mulesoft.com>
2022-07-14 15:35:16 -0300

    Chain is created for every child so it can be intercepted

--- src/main/java/org/mule/extension/validation/internal/privileged/AggregateOperationExecutor.java
+++ src/main/java/org/mule/extension/validation/internal/privileged/AggregateOperationExecutor.java
@@ -7,59 +7,88 @@
 package org.mule.extension.validation.internal.privileged;
 
 import static java.util.Optional.ofNullable;
+
+import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.initialiseIfNeeded;
+import static org.mule.runtime.core.privileged.processor.MessageProcessors.newChain;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.newChildContext;
 import static org.mule.runtime.extension.api.error.MuleErrors.VALIDATION;
 
 import org.mule.runtime.api.component.location.ComponentLocation;
+import org.mule.runtime.api.lifecycle.InitialisationException;
 import org.mule.runtime.api.message.Error;
 import org.mule.runtime.api.message.ErrorType;
 import org.mule.runtime.api.meta.model.operation.OperationModel;
+import org.mule.runtime.core.api.MuleContext;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.processor.Processor;
 import org.mule.runtime.core.privileged.event.BaseEventContext;
 import org.mule.runtime.core.privileged.exception.EventProcessingException;
 import org.mule.runtime.core.privileged.processor.chain.HasMessageProcessors;
+import org.mule.runtime.core.privileged.processor.chain.MessageProcessorChain;
 import org.mule.runtime.extension.api.runtime.operation.CompletableComponentExecutor;
 import org.mule.runtime.extension.api.runtime.operation.ExecutionContext;
 import org.mule.runtime.module.extension.api.runtime.privileged.ExecutionContextAdapter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Field;
 import java.util.Optional;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 
 abstract class AggregateOperationExecutor implements CompletableComponentExecutor<OperationModel> {
 
   public void execute(ExecutionContext<OperationModel> executionContext, ExecutorCallback callback) {
 
+    MuleContext muleContext = null;
+    try {
+      Field field = executionContext.getClass().getDeclaredField("muleContext");
+      field.setAccessible(true);
+      muleContext = (MuleContext) field.get(executionContext);
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (NoSuchFieldException e) {
+      e.printStackTrace();
+    }
+
     HasMessageProcessors chain = executionContext.getParameter("validations");
+
     final ExecutionContextAdapter<OperationModel> context = (ExecutionContextAdapter<OperationModel>) executionContext;
     final CoreEvent event = context.getEvent();
     final Optional<ComponentLocation> location = ofNullable(context.getComponent().getLocation());
 
-    List<Error> errors = new ArrayList<>(chain.getMessageProcessors().size());
+    List<Error> errorList = new ArrayList<>(chain.getMessageProcessors().size());
     for (Processor processor : chain.getMessageProcessors()) {
+      List<Processor> processorList = Collections.singletonList(processor);
+      MessageProcessorChain messageChain = newChain(Optional.empty(),
+                                                    processorList);
       BaseEventContext childContext = newChildContext(event, location);
       final CoreEvent processEvent = CoreEvent.builder(childContext, event).build();
       try {
-        CoreEvent result = processor.process(processEvent);
+        initialiseIfNeeded(messageChain, muleContext);
+        CoreEvent result = messageChain.process(processEvent);
         childContext.success(result);
+      } catch (InitialisationException e) {
+        childContext.error(e);
+        callback.error(e);
       } catch (EventProcessingException e) {
         childContext.error(e);
-        Error error = e.getEvent().getError().orElse(null);
+        Error error =
+            e.getEvent().getError().orElse(null);
         if (error != null && isValidation(error.getErrorType())) {
-          errors.add(error);
-        } else {
-          // propagated error must have its event tied to the context of the originally passed event, not a child
-          callback.error(new EventProcessingException(CoreEvent.builder(event.getContext(), event)
-              .error(error)
-              .build(), e.getCause()));
+          errorList.add(error);
+        } else { // propagated error must have its event tied to the context of the originally passed event, not a child
+          callback.error(new EventProcessingException(CoreEvent.builder(event.getContext(), event).error(error).build(),
+                                                      e.getCause()));
         }
       } catch (Exception e) {
         childContext.error(e);
         callback.error(e);
       }
     }
-    handleValidationErrors(callback, chain, errors);
+
+    handleValidationErrors(callback,
+                           chain, errorList);
+
   }
 
   protected abstract void handleValidationErrors(ExecutorCallback callback, HasMessageProcessors chain, List<Error> errors);
