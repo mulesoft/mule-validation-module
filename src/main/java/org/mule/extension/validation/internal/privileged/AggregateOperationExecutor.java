54c298c7ebc64b4dba36b183ee0e3d170cd932a5
Francisco Nery <fnery@mulesoft.com>
2022-07-15 12:22:57 -0300

    Inject instead of contextAware, and lifecyccle utils

--- src/main/java/org/mule/extension/validation/internal/privileged/AggregateOperationExecutor.java
+++ src/main/java/org/mule/extension/validation/internal/privileged/AggregateOperationExecutor.java
@@ -8,17 +8,17 @@ package org.mule.extension.validation.internal.privileged;
 
 import static java.util.Optional.ofNullable;
 
-import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.initialiseIfNeeded;
+import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.*;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.newChain;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.newChildContext;
 import static org.mule.runtime.extension.api.error.MuleErrors.VALIDATION;
+import static org.slf4j.LoggerFactory.getLogger;
 
 import org.mule.runtime.api.component.location.ComponentLocation;
 import org.mule.runtime.api.message.Error;
 import org.mule.runtime.api.message.ErrorType;
 import org.mule.runtime.api.meta.model.operation.OperationModel;
 import org.mule.runtime.core.api.MuleContext;
-import org.mule.runtime.core.api.context.MuleContextAware;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.processor.Processor;
 import org.mule.runtime.core.privileged.event.BaseEventContext;
@@ -28,19 +28,20 @@ import org.mule.runtime.core.privileged.processor.chain.MessageProcessorChain;
 import org.mule.runtime.extension.api.runtime.operation.CompletableComponentExecutor;
 import org.mule.runtime.extension.api.runtime.operation.ExecutionContext;
 import org.mule.runtime.module.extension.api.runtime.privileged.ExecutionContextAdapter;
+import org.slf4j.Logger;
 
+import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-abstract class AggregateOperationExecutor implements CompletableComponentExecutor<OperationModel>, MuleContextAware {
+abstract class AggregateOperationExecutor implements CompletableComponentExecutor<OperationModel> {
 
+  private static final Logger logger = getLogger(AggregateOperationExecutor.class);
+
+  @Inject
   private MuleContext muleContext;
-
-  public void setMuleContext(MuleContext context) {
-    this.muleContext = context;
-  }
 
   public void execute(ExecutionContext<OperationModel> executionContext, ExecutorCallback callback) {
 
@@ -60,6 +61,7 @@ abstract class AggregateOperationExecutor implements CompletableComponentExecuto
       try {
         // The chain is initialized with the muleContext so that it can run correctly
         initialiseIfNeeded(messageChain, muleContext);
+        startIfNeeded(messageChain);
         CoreEvent result = messageChain.process(processEvent);
         childContext.success(result);
       } catch (EventProcessingException e) {
@@ -77,8 +79,18 @@ abstract class AggregateOperationExecutor implements CompletableComponentExecuto
         childContext.error(e);
         callback.error(e);
       }
+      stopChain(messageChain);
     }
     handleValidationErrors(callback, chain, errors);
+  }
+
+  private void stopChain(MessageProcessorChain chain) {
+    try {
+      stopIfNeeded(chain);
+    } catch (Exception e) {
+      logger.debug("Exception found trying to stop the chain");
+    }
+    disposeIfNeeded(chain, logger);
   }
 
   protected abstract void handleValidationErrors(ExecutorCallback callback, HasMessageProcessors chain, List<Error> errors);
