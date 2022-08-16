010df044d363b6b2be47066558d8a68b9ca526f2
francisco-nery <87720704+francisco-nery@users.noreply.github.com>
2022-08-16 10:48:34 -0300

    Chore/cherrypicks 20221010 (#212)
    * W-11063824: ignore sdk update in renovate (#203)
    
    (cherry picked from commit 915add4391743e5e67ba1242e509d283dcf216ef)
    
    * Update dependency org.mule.connectors:mule-file-connector to v1.4.1
    
    (cherry picked from commit 63d66fa98ec474852cdc873c7d8160cda1ea440e)
    
    * Update munit.version to v2.3.12
    
    * Update dependency org.apache.maven.plugins:maven-resources-plugin to v3.3.0
    
    (cherry picked from commit cac6ad5b0b1301ab61b26280dd545bdfce471ce4)
    
    * W-11495919: Added the minMuleVersion for the MTF tests
    
    (cherry picked from commit 92dce55ef28991867b38ff072f41de5f8d48c535)
    
    * Fix/w 10900821 (#207)
    
    * Chain is created for every child so it can be intercepted
    
    * Import orderr
    
    * Remove reflection, comments
    
    * Errors
    
    * Comment
    
    * Inject instead of contextAware, and lifecyccle utils
    
    * Imports
    
    (cherry picked from commit 2ce9040e3c19272a4c19aede274d996d1fd2a498)
    
    Co-authored-by: Ignacio Giss <rgiss@mulesoft.com>
    Co-authored-by: Engineering Services Bot <engineering-services-bot@mulesoft.com>
    Co-authored-by: Alejandro Curci <acurci@mulesoft.com>

--- src/main/java/org/mule/extension/validation/internal/privileged/AggregateOperationExecutor.java
+++ src/main/java/org/mule/extension/validation/internal/privileged/AggregateOperationExecutor.java
@@ -7,59 +7,94 @@
 package org.mule.extension.validation.internal.privileged;
 
 import static java.util.Optional.ofNullable;
+
+import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.disposeIfNeeded;
+import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.initialiseIfNeeded;
+import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.startIfNeeded;
+import static org.mule.runtime.core.api.lifecycle.LifecycleUtils.stopIfNeeded;
+import static org.mule.runtime.core.privileged.processor.MessageProcessors.newChain;
 import static org.mule.runtime.core.privileged.processor.MessageProcessors.newChildContext;
 import static org.mule.runtime.extension.api.error.MuleErrors.VALIDATION;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 import org.mule.runtime.api.component.location.ComponentLocation;
 import org.mule.runtime.api.message.Error;
 import org.mule.runtime.api.message.ErrorType;
 import org.mule.runtime.api.meta.model.operation.OperationModel;
+import org.mule.runtime.core.api.MuleContext;
 import org.mule.runtime.core.api.event.CoreEvent;
 import org.mule.runtime.core.api.processor.Processor;
 import org.mule.runtime.core.privileged.event.BaseEventContext;
 import org.mule.runtime.core.privileged.exception.EventProcessingException;
 import org.mule.runtime.core.privileged.processor.chain.HasMessageProcessors;
+import org.mule.runtime.core.privileged.processor.chain.MessageProcessorChain;
 import org.mule.runtime.extension.api.runtime.operation.CompletableComponentExecutor;
 import org.mule.runtime.extension.api.runtime.operation.ExecutionContext;
 import org.mule.runtime.module.extension.api.runtime.privileged.ExecutionContextAdapter;
+import org.slf4j.Logger;
 
+import javax.inject.Inject;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 abstract class AggregateOperationExecutor implements CompletableComponentExecutor<OperationModel> {
 
+  private static final Logger logger = getLogger(AggregateOperationExecutor.class);
+
+  @Inject
+  private MuleContext muleContext;
+
   public void execute(ExecutionContext<OperationModel> executionContext, ExecutorCallback callback) {
 
     HasMessageProcessors chain = executionContext.getParameter("validations");
+
     final ExecutionContextAdapter<OperationModel> context = (ExecutionContextAdapter<OperationModel>) executionContext;
     final CoreEvent event = context.getEvent();
     final Optional<ComponentLocation> location = ofNullable(context.getComponent().getLocation());
 
     List<Error> errors = new ArrayList<>(chain.getMessageProcessors().size());
     for (Processor processor : chain.getMessageProcessors()) {
+      // A new chain is created for each processor so that they can be intercepted by MUnit
+      MessageProcessorChain messageChain = newChain(Optional.empty(),
+                                                    Collections.singletonList(processor));
       BaseEventContext childContext = newChildContext(event, location);
       final CoreEvent processEvent = CoreEvent.builder(childContext, event).build();
       try {
-        CoreEvent result = processor.process(processEvent);
+        // The chain is initialized with the muleContext so that it can run correctly
+        initialiseIfNeeded(messageChain, muleContext);
+        startIfNeeded(messageChain);
+        CoreEvent result = messageChain.process(processEvent);
         childContext.success(result);
       } catch (EventProcessingException e) {
         childContext.error(e);
-        Error error = e.getEvent().getError().orElse(null);
+        Error error =
+            e.getEvent().getError().orElse(null);
         if (error != null && isValidation(error.getErrorType())) {
           errors.add(error);
         } else {
           // propagated error must have its event tied to the context of the originally passed event, not a child
-          callback.error(new EventProcessingException(CoreEvent.builder(event.getContext(), event)
-              .error(error)
-              .build(), e.getCause()));
+          callback.error(new EventProcessingException(CoreEvent.builder(event.getContext(), event).error(error).build(),
+                                                      e.getCause()));
         }
       } catch (Exception e) {
         childContext.error(e);
         callback.error(e);
       }
+      stopChain(messageChain);
     }
     handleValidationErrors(callback, chain, errors);
+  }
+
+  private void stopChain(MessageProcessorChain chain) {
+    try {
+      stopIfNeeded(chain);
+    } catch (Exception e) {
+      logger.debug("Exception found trying to stop the chain");
+    }
+    disposeIfNeeded(chain, logger);
   }
 
   protected abstract void handleValidationErrors(ExecutorCallback callback, HasMessageProcessors chain, List<Error> errors);
@@ -75,5 +110,4 @@ abstract class AggregateOperationExecutor implements CompletableComponentExecuto
 
     return isValidation(errorType.getParentErrorType());
   }
-
 }
